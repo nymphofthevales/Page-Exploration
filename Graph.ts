2287ae7d945a37b7d7dc64bc5916fd5c796c0b5d
nymphofthevales <81981344+nymphofthevales@users.noreply.github.com>
2022-03-21 18:42:37 -0700

    write data structure
    leaf (data)-> Sequence/StoryNode (GraphNode) -> StoryManager (Graph)

--- Graph.ts
+++ Graph.ts
@@ -1,28 +1,61 @@
 
-
-
 /**
  * A data structure for representing any set of interconnected nodes.
 */
-export class Graph {
-    nodes: Array<GraphNode> = []
+class Graph {
+    nodes: Map<any,GraphNode> = new Map()
     constructor () {
     }
-    add(node: GraphNode) {
-        this.nodes.push(node)
+    node(index: string): GraphNode {
+        let node = this.nodes.get(index)
+        if (!node) {
+            node = this.newNode(index)
+        }
+        return node
     }
-    link(a: GraphNode, b: GraphNode) {
-        a.link(b)
-        b.link(a)
+    add(node: GraphNode): GraphNode {
+        let index = node.index
+        this.nodes.set( index, node )
+        return this.node(index)
+    }
+    /**
+     * Initialize a new node in the graph.
+    */
+    newNode(index: string): GraphNode {
+        index = this.checkFalsyIndex(index)
+        this.nodes.set( index, new GraphNode(index) )
+        return this.node(index)
+    }
+    /**
+     * Initialize multiple nodes at once by providing a list of names.
+    */
+    newNodes(indices:Array<any> = []) {
+        for (let i=0; i < indices.length; i++) {
+            this.newNode(indices[i])
+        }
+    }
+    checkFalsyIndex(index: string | undefined): string {
+        if (!index) {
+            index = (this.nodes.size + 1).toString()
+        }
+        return index
+    }
+    link(a: string, b: string) {
+        let node1 = this.node(a)
+        let node2 = this.node(b)
+        node1.link(node2)
+        node2.link(node1)
     }
 }
 
 /**
  * A node in a graph. Can be connected to other nodes.
 */
-export class GraphNode {
+class GraphNode {
+    index: string
     connections: Array<GraphNode> = []
-    constructor () {
+    constructor (index: string) {
+        this.index = index
     }
     link(node: GraphNode) {
         this.connections.push(node)
@@ -32,11 +65,13 @@ export class GraphNode {
 /**
  * A 'directed' graph, with one-way connections by default.
  */
-export class DiGraph extends Graph {
+class DiGraph extends Graph {
     constructor () {
         super()
     }
-    link(origin: GraphNode, destination: GraphNode) {
-        origin.link(destination)
+    link(origin: string, destination: string) {
+        let o = this.node(origin)
+        let d = this.node(destination)
+        o.link(d)
     }
 }
--- entrypoint.ts
+++ entrypoint.ts
@@ -1,12 +1,19 @@
 
-import { DiGraph, GraphNode } from "./Graph"
+//import { DiGraph, GraphNode } from "./Graph"
 
 /**
  * A collection of all the pages in a story.
 */
 class StoryManager extends DiGraph {
+    nodes: Map<string, Sequence | StoryNode> = new Map()
     constructor () {
         super()
+    }
+    newSequence(opts: SequenceOptions) {
+        this.add(new Sequence(opts, this))
+    }
+    newStoryNode(opts: StoryNodeOptions) {
+        this.add(new StoryNode(opts, this))
     }
 }
 
@@ -14,10 +21,12 @@ class StoryManager extends DiGraph {
  * Holds text and options for a page in a branching story.
 */
 class PageBranch extends GraphNode {
-    title: string
-    constructor (title: string) {
-        super()
-        this.title = title;
+    connections: Array<PageBranch> = []
+    leaves: Array<Leaf> = []
+    story: StoryManager
+    constructor (title: string, story: StoryManager) {
+        super(title)
+        this.story = story
     }
 }
 
@@ -25,37 +34,91 @@ class PageBranch extends GraphNode {
  * A single text-option set.
 */
 class Leaf {
-    constructor(passage: string, options: Array<string>) {
-
+    passage: string
+    choices: Array<string>
+    constructor(passage: string, choices: Array<string>) {
+        this.passage = passage
+        this.choices = choices
     }
 }
 
+
+interface SequenceOptions {
+    title: string,
+    passages: Array<string>,
+    defaultChoice: string
+}
 /**
  * A series of ordered passages of text, each only being connected to the next without branching.
 */
 class Sequence extends PageBranch {
-    leaves: Array<Leaf> = []
-    constructor (title: string) {
-        super(title)
+    constructor (opts: SequenceOptions, story: StoryManager) {
+        let {title, passages, defaultChoice} = opts
+        super(title, story)
+        this.addBatchPage(passages, defaultChoice)
     }
     get length() {
         return this.leaves.length
     }
-    addPage(passage: string, option: string) {
-        this.leaves.push(new Leaf(passage, [option]))
+    addPage(passage: string, choice: string) {
+        this.leaves.push(new Leaf(passage, [choice]))
     }
-    addBatchPage(passages: Array<string>, option: string) {
+    addBatchPage(passages: Array<string>, defaultChoice: string) {
         for (let i=0; i < passages.length; i++) {
-            this.leaves.push(new Leaf(passages[i], [option]))
+            this.leaves.push(new Leaf(passages[i], [defaultChoice]))
         }
     }
+    link(branch: PageBranch) {
+        this.connections = [branch]
+    }
+}
+
+type NodeTitle = string
+
+interface StoryNodeOptions {
+    title: NodeTitle,
+    passage: string,
+    choices: Array<[string, NodeTitle]>
 }
 
 /**
  * A single page with multiple options; which connects to multiple branches.
 */
 class StoryNode extends PageBranch {
-    constructor () {
-        super()
+    leaf: Leaf
+    constructor (opts: StoryNodeOptions, story: StoryManager) {
+        let {title, passage, choices} = opts
+        super(title, story)
+        this.story = story
+        this.leaf = new Leaf(passage,[])
+        for (let i=0; i < choices.length; i++) {
+            let [choice, destination] = choices[i]
+            this.addOption(choice, destination)
+        }
+        
+    }
+    addOption(choice: string, destination: string) {
+        let dest = this.story.node(destination)
+        this.leaf.choices.push(choice)
+        this.link(dest)
+    }
+    set text(passage: string) {
+        this.leaf.passage = passage
+    }
+    get options() {
+        let opts: Array<[string, PageBranch]> = []
+        for (let i=0; i < this.leaf.choices.length; i++) {
+            let optionText = this.leaf.choices[i]
+            let optionDestination = this.connections[i]
+            opts.push( [optionText, optionDestination] )
+        }
+        return opts
     }
 }
+
+let labyrinth = new StoryManager()
+labyrinth.newStoryNode({
+    title: 'entryways',
+    passage: '',
+    choices: []
+})
